4e17cac16c9d58a3c0a5150f16e779979b27fb7f
myungmolee <lukei321@kakao.com>
2024-01-03 16:18:27 +0900

    update

--- src/main/java/com/example/TodoApp/controller/LoginController.java
+++ src/main/java/com/example/TodoApp/controller/LoginController.java
@@ -2,6 +2,7 @@ package com.example.TodoApp.controller;
 
 import com.example.TodoApp.service.AccountService;
 import com.example.TodoApp.vo.LoginVO;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
@@ -9,13 +10,13 @@ import org.springframework.web.bind.annotation.PostMapping;
 
 import java.util.Optional;
 
-//@Controller
-//public class LoginController {
+@Controller
+public class LoginController {
 //    private final AccountService accountService = new AccountService();
-//    @GetMapping("/login")
-//    public String login() {
-//        return "login";
-//    }
+    @GetMapping("/login")
+    public String login() {
+        return "login";
+    }
 
 //    @PostMapping("/login")
 //    public String loginSuccess(@ModelAttribute LoginVO loginVO) {
@@ -23,4 +24,4 @@ import java.util.Optional;
 //
 //        return "home";
 //    }
-//}
+}
